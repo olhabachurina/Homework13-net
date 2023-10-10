4ead8f9e1138ceb0de22a2e2ae20bf7b59c3ba1c
Olga Bachurina <bachuri12@ukr.net>
2023-10-10 11:50:17 +0300

    Add project files.

---
+++ ArithmeticOperations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework13_net
+{
+    public delegate double ArithmeticOperation(double x, double y);
+
+
+    public class ArithmeticOperations
+    {
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        
+        public static double PerformOperation(ArithmeticOperation operation, double x, double y)// Метод для выполнения операции через делегат
+        {
+            return operation(x, y);
+        }
+        public static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+
+        public static bool IsOdd(int n)
+        {
+            return n % 2 != 0;
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsFibonacci(int n)
+        {
+            return IsPerfectSquare(5 * n * n + 4) || IsPerfectSquare(5 * n * n - 4);
+        }
+
+        
+        private static bool IsPerfectSquare(int x)// Метод для проверки наличия квадратного корня
+        {
+            int sqrt = (int)Math.Sqrt(x);
+            return sqrt * sqrt == x;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Homework13_net;
+using System;
+
+Console.WriteLine("Exercise 2");
+
+
+
+ArithmeticOperation addition = ArithmeticOperations.Add;
+ArithmeticOperation subtraction = ArithmeticOperations.Subtract;
+ArithmeticOperation multiplication = ArithmeticOperations.Multiply;
+
+// Ввод чисел
+Console.Write("Enter first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+// Выполнение операций
+Console.WriteLine($"Addition result: {ArithmeticOperations.PerformOperation(addition, num1, num2)}");
+Console.WriteLine($"Subtraction result: {ArithmeticOperations.PerformOperation(subtraction, num1, num2)}");
+Console.WriteLine($"Result of multiplication: {ArithmeticOperations.PerformOperation(multiplication, num1, num2)}");
+Console.ReadLine();
+Console.WriteLine("Exercise 3");
+Predicate<int> isEven = ArithmeticOperations.IsEven;//Создание делегата для проверки нчетности
+Predicate<int> isOdd = ArithmeticOperations.IsOdd;// Создание делегата для проверки нечетности
+Predicate<int> isPrime = ArithmeticOperations.IsPrime;// Создание делегата для проверки простого числа
+Predicate<int> isFibonacci = ArithmeticOperations.IsFibonacci;// Создание делегата для проверки числа Фибоначчи
+Console.Write("Enter an integer: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{number} is even: {isEven.Invoke(number)}");
+Console.WriteLine($"{number} is odd: {isOdd.Invoke(number)}");
+Console.WriteLine($"{number} is a prime number: {isPrime.Invoke(number)}");
+Console.WriteLine($"{number} is a Fibonacci number: {isFibonacci.Invoke(number)}");
+
+Console.ReadLine();
+
+Console.WriteLine("Exercise 4");
+
+Console.WriteLine($"Addition result: {addition.Invoke(num1, num2)}");
+Console.WriteLine($"ubtraction result: {subtraction.Invoke(num1, num2)}");
+Console.WriteLine($"Result of multiplication: {multiplication.Invoke(num1, num2)}");
+
+Console.ReadLine();
+    
